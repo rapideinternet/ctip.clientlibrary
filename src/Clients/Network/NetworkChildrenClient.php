fa7cfd7a00fb2f44192cf4e706200a04484f06da
Siemen Boelkens <siemen@rapide.software>
2018-09-19 15:24:59 +0200

    small bug fix

--- src/Clients/Network/NetworkChildrenClient.php
+++ src/Clients/Network/NetworkChildrenClient.php
@@ -60,15 +60,16 @@ class NetworkChildrenClient extends NestedClient
 
         return $this->request(vsprintf('network/%s/children/type/%s', $this->selectedId));
     }
-    
+
     /**
      * @return mixed
      */
     public function allChildren()
     {
         $this->addParameter('perPage', PHP_INT_MAX);
+        $this->addParameter('page', 1);
 
-        return $this->request(vsprintf('object/%s/children/all', $this->selectedId));
+        return $this->request(vsprintf('network/%s/children/all', $this->selectedId));
     }
 
     /**
